d233076d8518d10d252718d5e912294370ae3cbd
atimchik <indatim@gmail.com>
2024-01-04 22:44:28 +0200

    Add @media variables

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,10 @@
 const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
+const headerModalBtnOpen = document.querySelector('.header__button');
+const heroModalBtnOpen = document.querySelector('.hero-section__button');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
-modalBtnOpen.addEventListener('click', toggleModal);
+headerModalBtnOpen.addEventListener('click', toggleModal);
+heroModalBtnOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
