e6a5d3cf2f5910c943ff218dd4a961b801268287
atimchik <indatim@gmail.com>
2024-01-08 13:00:46 +0200

    Add mobile-modal menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,6 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuBtnOpen = document.querySelector('.menu-btn-open');
-const menuBtnClose = document.querySelector('.menu-btn-close');
+const menuBtnClose = document.querySelector('.mobile-menu__btn-close');
 
 const toggleMenu = () => mobileMenu.classList.toggle('is-open');
 const disableScroll = () =>
